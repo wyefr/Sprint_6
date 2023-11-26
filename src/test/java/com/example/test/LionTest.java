ac4c0d8a1a95a96c8651b7a41de18dfc0df0670e
Georgiy Kuznetsov <georgiy.kuznetsov@onetwotrip.com>
2023-11-26 17:20:14 +0400

    Deleted testDoesHaveMane() and testDoesHaveManeForMale() from LionTest.java

--- src/test/java/com/example/test/LionTest.java
+++ src/test/java/com/example/test/LionTest.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,38 +19,27 @@ public class LionTest {
     @Mock
     private Feline felineMock;
 
-    @Test
-    public void testDoesHaveMane() {
-        Lion lion = Lion.getInstance("Самец", felineMock);
-        assertTrue(lion.doesHaveMane());
-    }
-    @Test
-    public void testDoesHaveManeForMale() {
-        Lion lion = Lion.getInstance("Самец", felineMock);
-        assertTrue(lion.doesHaveMane());
-    }
-
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidSex() {
-        Lion.getInstance("Недопустимое значение пола", felineMock);
+        new Lion("Недопустимое значение пола", felineMock);
     }
 
     @Test(expected = IllegalStateException.class)
     public void testGetKittensWithoutFeline() {
-        Lion lion = Lion.getInstance("Самка", null);
+        Lion lion = new Lion("Самка", null);
         lion.getKittens();
     }
 
     @Test
     public void testGetKittens() {
         when(felineMock.getKittens()).thenReturn(3);
-        Lion lion = Lion.getInstance("Самец", felineMock);
+        Lion lion = new Lion("Самец", felineMock);
         assertEquals(3, lion.getKittens());
     }
 
     @Test
     public void testGetFood() throws Exception {
-        Lion lion = Lion.getInstance("Самец", felineMock);
+        Lion lion = new Lion("Самец", felineMock);
         List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
         when(felineMock.getFood("Хищник")).thenReturn(expectedFood);
         List<String> food = lion.getFood();
@@ -60,7 +48,7 @@ public class LionTest {
 
     @Test(expected = IllegalStateException.class)
     public void testGetFoodWithoutFeline() throws Exception {
-        Lion lion = Lion.getInstance("Самец", null);
+        Lion lion = new Lion("Самец", null);
         lion.getFood();
     }
 }
